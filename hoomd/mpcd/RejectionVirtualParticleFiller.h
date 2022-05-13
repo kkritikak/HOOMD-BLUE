e7c29bdd2cb0327e979fe90d056fcdead9da82e4
YMWani <ymanishw@uni-mainz.de>
2022-05-13 19:49:12 +0200

    geometry name

--- hoomd/mpcd/RejectionVirtualParticleFiller.h
+++ hoomd/mpcd/RejectionVirtualParticleFiller.h
@@ -41,6 +41,7 @@ class PYBIND11_EXPORT RejectionVirtualParticleFiller : public mpcd::VirtualParti
                                        std::shared_ptr<const Geometry> geom)
         : mpcd::VirtualParticleFiller(sysdata, density, type, T, seed), m_geom(geom)
         {
+        const std::string name = Geometry::getName() + "RejectionFiller";
         m_exec_conf->msg->notice(5) << "constructing MPCD RejectionVirtualParticleFiller" << std::endl;
         }
 
