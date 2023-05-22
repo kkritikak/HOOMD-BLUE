89b8eea8d5b7956df8af8e695c46f3d23de043be
KRITIKA <kkritika@uni-mainz.de>
2023-05-22 12:18:07 +0200

    modified some comments and change m_R to m_R0 variable

--- hoomd/mpcd/SphereGeometryRetracting.h
+++ hoomd/mpcd/SphereGeometryRetracting.h
@@ -22,12 +22,12 @@ namespace detail
 {
 //! Sphere geometry
 /*!
- * This models a fluid confined inside a sphere, centered at the origin and with radius R.
+ * This models a fluid confined inside a sphere, centered at the origin and with initial radius R.
  *
  * If a particle leaves the sphere in a single simulation step, the particle is backtracked to the point on the
  * surface from which it exited the surface and then reflected according to appropriate boundary condition.
  */
-class __attribute__((visibility("default"))) SphereGeometry
+class __attribute__((visibility("default"))) SphereGeometryRetracting
     {
     public:
         //! Constructor
@@ -36,13 +36,12 @@ class __attribute__((visibility("default"))) SphereGeometry
          * \param bc Boundary condition at the wall (slip or no-slip)
 	 * \param V Velocity of spherical container
 	 * \param t is time
-	 * \\m_R0 is the radius of container at time t, R = R0 - V*(t-t'), t' is the initial time(taking it as zero)
-	 * \so R = R0 -V*t
-	 * \m_R is the radius of spherical container at that time step
-	 * \m_R2 is R0*R0
+	 * \\m_R0 is the radius(R0) of container at time t, R = R0 - V*(t-t'), t' is the initial time(taking it as zero)
+	 * \so R = R0 -V*t and R0 = R + V*t
+	 * \m_R02 is R0*R0
          */
         HOSTDEVICE SphereGeometry(Scalar R, Scalar V, Scalar t, boundary bc)
-            : m_R(R+V*t), m_R2((R+V*t)*(R+V*t)), m_bc(bc), m_V(V), m_V2(V*V)
+            : m_R0(R+V*t), m_R02((R+V*t)*(R+V*t)), m_bc(bc), m_V(V), m_V2(V*V), m_R(R)
             { }
 
         //! Detect collision between the particle and the boundary
@@ -66,7 +65,7 @@ class __attribute__((visibility("default"))) SphereGeometry
              */
             const Scalar r2 = dot(pos,pos);
             const Scalar v2 = dot(vel,vel);
-            if (r2 <= m_R2 || v2 == Scalar(0))
+            if (r2 <= m_R02 || v2 == Scalar(0))
                {
                dt = Scalar(0);
                return false;
@@ -81,8 +80,9 @@ class __attribute__((visibility("default"))) SphereGeometry
              */
 
             const Scalar rv = dot(pos,vel);
-	    const Scalar RV = m_R*m_V;
-            dt = ((rv-RV) – fast::sqrt((rv-RV)*(rv-RV)-(v2-m_V2)*(r2-m_R2)))/(v2-m_V2);
+	    const Scalar RV = m_R0*m_V;   //RV is R0*V
+	    const Scalar rv_RV = rv - RV;  //rv_RV is rv - RV
+            dt = (rv_RV – fast::sqrt(rv_RV*rv_RV-(v2-m_V2)*(r2-m_R02)))/(v2-m_V2);
 
             // backtrack the particle for time dt to get to point of contact
             pos -= vel*dt;
@@ -97,7 +97,8 @@ class __attribute__((visibility("default"))) SphereGeometry
              */
             if (m_bc == boundary::no_slip)
                 {
-                /* No-slip and no penetration requires reflection of both parallel and perpendicular components.
+                /* No-slip and no penetration requires reflection of both parallel and perpendicular components 
+		 * v'= -vel + V_interface 
                  * This results in just flipping of all the velocity components.
 		 * V_v is the velocity of spherical geometry in vector form which is V*normal vector along surface
 		 * V_V = V*(normal vector calculated from pos) = V*(pos/sqrt(pos.pos))
@@ -110,11 +111,11 @@ class __attribute__((visibility("default"))) SphereGeometry
                 /*
                  * Only no-penetration condition is enforced, so only v_perp is reflected.
                  * The new velocity v' is:
-                 * v' = -v_perp + v_para = v - 2*v_perp
+                 * v' = -v_perp + v_para + V_interface = v - 2*v_perp + V_interface
                 */
 		const Scalar3 V_v = (m_V*pos/(fast::sqrt(r2)));
-                const Scalar3 vperp = (dot(vel,pos)/m_R2)*pos;
-                vel -= Scalar(2)*vperp + V_v;
+                const Scalar3 vperp = (dot(vel,pos)/m_R02)*pos;
+                vel -= Scalar(2)*vperp - V_v;
                 }
             return true;
             }
@@ -126,7 +127,7 @@ class __attribute__((visibility("default"))) SphereGeometry
          */
         HOSTDEVICE bool isOutside(const Scalar3& pos) const
             {
-            return dot(pos,pos) > m_R2;
+            return dot(pos,pos) > m_R02;
             }
 
         //! Validate that the simulation box is large enough for the geometry
@@ -158,7 +159,7 @@ class __attribute__((visibility("default"))) SphereGeometry
          */
         HOSTDEVICE Scalar getR() const
             {
-            return m_R;
+            return m_R0;
             }
 
         //! Get the wall boundary condition
@@ -174,16 +175,17 @@ class __attribute__((visibility("default"))) SphereGeometry
         //! Get the unique name of this geometry
         static std::string getName()
             {
-            return std::string("Sphere");
+            return std::string("SphereRetracting");
             }
         #endif // NVCC
 
     private:
-        const Scalar m_R;       //!< Sphere radius
-        const Scalar m_R2;      //!< Square of sphere radius
+        const Scalar m_R0;       //!< Sphere radius
+        const Scalar m_R02;      //!< Square of sphere radius
         const boundary m_bc;    //!< Boundary condition
 	const Scalar m_V;       //!<Velocity of container
 	const Scalar m_V2;      //!<square of velocity of spherical container
+	const Scalar m_R;      //!initial radius of spherical container
     };
 
 } // end namespace detail
