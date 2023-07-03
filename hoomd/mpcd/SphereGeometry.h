0d8eae474f7d854ebaf8a5adbc76e26f5840210e
KRITIKA <kkritika@uni-mainz.de>
2023-07-03 11:45:13 +0200

    modifying std::fabs instead of fabs

--- hoomd/mpcd/SphereGeometry.h
+++ hoomd/mpcd/SphereGeometry.h
@@ -105,7 +105,7 @@ class __attribute__((visibility("default"))) SphereGeometry
              *when v2 - V2 ~ 0, different formula(calculated by (lim(v->V)dt)) is used
              */
 
-            if (fabs(v2_minus_V2) < Scalar(1e-8))
+            if (std::fabs(v2_minus_V2) < Scalar(1e-8))
                 {
                 dt = (r2-m_R2)/(Scalar(2)*rv_RV);
                 }
