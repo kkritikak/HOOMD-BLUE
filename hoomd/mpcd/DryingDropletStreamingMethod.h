78c8d8168e6273984153ee2c7b97bb50e31fb0d2
KRITIKA <kkritika@uni-mainz.de>
2023-08-07 16:59:52 +0200

    changes for Counting total N_bounced and calculating N_remove(no. of particles to remove)

--- hoomd/mpcd/DryingDropletStreamingMethod.h
+++ hoomd/mpcd/DryingDropletStreamingMethod.h
@@ -52,9 +52,9 @@ class PYBIND11_EXPORT DryingDropletStreamingMethod : public mpcd::ConfinedStream
         DryingDropletStreamingMethod(std::shared_ptr<mpcd::SystemData> sysdata,
                                     unsigned int cur_timestep,
                                     unsigned int period,
-                                    int phase, std::shared_ptr<::Variant> R, boundary bc)
+                                    int phase, std::shared_ptr<::Variant> R, boundary bc, const Scalar rho, unsigned int seed)
         : mpcd::ConfinedStreamingMethod<mpcd::SphereGeometry>(sysdata, cur_timestep, period, phase, std::shared_ptr<mpcd::SphereGeometry>()),
-          m_R(R),m_bc(bc)
+          m_R(R), m_bc(bc), m_rho(rho), m_seed(seed)
           {}
 
         //! Implementation of the streaming rule
@@ -62,6 +62,9 @@ class PYBIND11_EXPORT DryingDropletStreamingMethod : public mpcd::ConfinedStream
     private:
         std::shared_ptr<::Variant> m_R; //!Radius of Sphere
         const boundary m_bc;            //!boundary conditions
+        const Scalar m_rho;             //!solvent density
+
+        GPUArray<unsigned int> m_bounced_index;          //!GPUArray for storing the indices or bounced particles
     };
 
 /*!
@@ -93,11 +96,138 @@ void DryingDropletStreamingMethod<mpcd::SphereGeometry>::stream(unsigned int tim
     //stream according to base class rules
     ConfinedStreamingMethod<mpcd::SphereGeometry>::stream(timestep);
     
-    //Removing the right amount of particles
-    ArrayHandle<unsigned char> h_bounced(m_bounced, access_location::host, access_mode::read);
+    //calculating number of particles to remove from solvent density
+    const unsigned int N_remove = round((4.*M_PI/3.)*end_R*end_R*end_R*m_rho);
+    const int N_evap = m_mpcd_pdata->getNGlobal() - N_remove;
     
+    // get the compact array of indexes of bounced particles
+    unsigned int N_bounced = 0;
+        {
+        const unsigned int N = m_pdata->getN();
+        ArrayHandle<unsigned char> h_bounced(m_bounced, access_location::host, access_mode::read);
+        bool overflowed = false;
+        do
+            {
+            if (overflowed)
+                {
+                GPUArray<unsigned char> bounced_index(N_bounced, m_exec_conf);
+                m_bounced_index.swap(bounced_index);
+                // resize m_bounced_index with swap idiom
+                }
+            ArrayHandle<unsigned int> h_bounced_index(m_bounced_index, access_location::host, access_mode::overwrite);
+
+            const unsigned int N_bounced_max = m_bounced_index.size();
+            N_bounced = 0;
+            for (unsigned int idx=0; idx < N; ++idx)
+                {
+                if (h_bounced.data[idx])
+                    {
+                    if (N_bounced < N_bounced_max)
+                        h_bounced_index.data[N_bounced] = idx;
+                    ++N_bounced;
+                    }
+                }
+            overflowed = N_bounced > N_bounced_max;
+            } while (overflowed)
+        }
+
+    // reduce / scan the number of particles that were bounced on all ranks
+    unsigned int N_bounced_total = N_bounced;
+    unsigned int N_before = 0;
+    #ifdef ENABLE_MPI
+    if (m_exec_conf->getNRanks() > 1)
+        {
+        MPI_Allreduce(&N_bounced, &N_bounced_total, 1, MPI_UNSIGNED, MPI_SUM, m_exec_conf->getMPICommunicator());
+        MPI_Exscan(&N_bounced, &N_before, 1, MPI_UNSIGNED, MPI_SUM, m_exec_conf->getMPICommunicator());
+        }
+    #endif // ENABLE_MPI
+
+    // N_remove is the total number to remove (computed previously)
+    if (N_remove < N_bounced_total)
+        {
+        // do the pick logic, otherwise we should remove all the marked ones
+        N_keep_total = N_bounced_total - N_remove;
+        unsigned int N_pick_total;
+        if (N_keep_total < N_bounced_total)
+            {
+            N_pick_total = N_keep_total;
+            //pick particles for N_keep
+            }
+        else
+            {
+            N_pick_total = N_bounced_total;
+            //pick particles for N_bounced
+            }
+        makeAllpicks(timestep, N_pick_total , N_bounced_total);
+        
+        /*
+         * Select the picks that lie on my rank, with reindexing to local mark indexes.
+         * This is performed in a do loop to allow for resizing of the GPUVector.
+         * After a short time, the loop will be ignored.
+         */
+        
+        const unsigned int max_pick_idx = N_before + N_bounced;
+        overflowed = false;
+        do
+            {
+            m_Npick = 0;
+            const unsigned int max_Npick = m_picks.size();
+
+                {
+                ArrayHandle<unsigned int> h_picks(m_picks, access_location::host, access_mode::overwrite);
+                for (unsigned int i=0; i < N_pick_total; ++i)
+                    {
+                    const unsigned int pick = m_all_picks[i];
+                    if (pick >= N_before && pick < max_pick_idx)
+                        {
+                        if (m_Npick < max_Npick)
+                            {
+                            h_picks.data[m_Npick] = pick - N_before;
+                            }
+                        ++m_Npick;
+                        }
+                    }
+                }
+
+            overflowed = (m_Npick > max_Npick);
+            if (overflowed)
+                {
+                m_picks.resize(m_Npick);
+                }
+
+            } while (overflowed);
+        }
     }
 
+void DryingDropletStreamingMethod::makeAllPicks(unsigned int timestep, unsigned int N_pick_total, unsigned int N_mark_total)
+    {
+    assert(N_pick_total <= N_mark_total);
+
+    // fill up vector which we will randomly shuffle
+    m_all_picks.resize(N_mark_total);
+    std::iota(m_all_picks.begin(), m_all_picks.end(), 0);
+
+    hoomd::RandomGenerator rng(hoomd::RNGIdentifier::DryingDropletStreamingMethod, m_seed, timestep);
+
+    // random shuffle (fisher-yates) to get picks, seeded the same across all ranks
+    auto begin = m_all_picks.begin();
+    auto end = m_all_picks.end();
+    size_t left = std::distance(begin,end);
+    unsigned int N_choose = N_pick_total;
+    while (N_choose-- && left > 1)
+        {
+        hoomd::UniformIntDistribution rand_shift(left-1);
+
+        auto r = begin;
+        std::advance(r, rand_shift(rng));
+        std::swap(*begin, *r);
+        ++begin;
+        --left;
+        }
+
+    // size the vector down to the number picked
+    m_all_picks.resize(N_pick_total);
+    }
 
 namespace detail
 {
@@ -111,7 +241,7 @@ void export_DryingDropletStreamingMethod(pybind11::module& m)
     namespace py = pybind11;
     py::class_<mpcd::DryingDropletStreamingMethod,mpcd::ConfinedStreamingMethod<mpcd::SphereGeometry>, std::shared_ptr<DryingDropletStreamingMethod>>
         (m, "DryingDropletStreamingMethod")
-        .def(py::init<std::shared_ptr<mpcd::SystemData>, unsigned int, unsigned int, int, std::shared_ptr<::Variant>, boundary>())
+        .def(py::init<std::shared_ptr<mpcd::SystemData>, unsigned int, unsigned int, int, std::shared_ptr<::Variant>, boundary, Scalar, unsigned int>())
     }
 } // end namespace detail
 } // end namespace mpcd
